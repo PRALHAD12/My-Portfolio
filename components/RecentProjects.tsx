31a49f2cede15cdb9c86057c849177f6ee9ed12d
Pralhad <truptigirishkale@gmail.com>
2024-09-02 17:12:14 +0530

    initialize some error

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -70,7 +70,7 @@ const RecentProjects = () => {
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
                     Check Live Site
-                  </p>
+                 </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
